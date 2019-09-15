1cc34db5ad27ae9138097804472e0f3678053e17
michelle <moyer.mj@gmail.com>
2019-09-15 17:35:46 -0400

    basic complete

---
+++ calculations/basic.js
@@ -0,0 +1,27 @@
+const basicData = `./data/Basic_Cupcake.txt`;
+const fs = require('fs');
+let basicCupcakes = fs.readFileSync(basicData).toString().split("\n")
+basicCupcakes.shift()
+
+const { convertToIntegers, yearly, weekly, monthly } = require(`./helpers.js`);
+let basicRevenue = [];
+let basicYearlyRevenue = [];
+let basicWeeklyRevenue = [];
+let basicMonthlyRevenue = [];
+
+let basicSold = [];
+let basicYearlySold = [];
+let basicWeeklySold = [];
+let basicMonthlySold = [];
+
+convertToIntegers(basicCupcakes, basicRevenue, 5);
+convertToIntegers(basicCupcakes, basicSold, 1);
+
+weekly(basicRevenue, basicWeeklyRevenue);
+monthly(basicRevenue, basicMonthlyRevenue);
+yearly(basicRevenue, basicYearlyRevenue);
+
+weekly(basicSold, basicWeeklySold);
+monthly(basicSold, basicMonthlySold);
+yearly(basicSold, basicYearlySold);
+
